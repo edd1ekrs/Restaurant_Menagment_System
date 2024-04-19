9b2aacbb177eaccb9b3514caca17f4a760f1a152
edd1ekrs <edi04.ek@gmail.com>
2024-04-20 01:42:58 +0200

    Shtimi i css dhe disa ndryshime ne app.js

--- restaurant_managment_system/src/App.js
+++ restaurant_managment_system/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import AppHeader from './components/header';
+import AppFooter from './components/footer';
+import AppHero from './components/hero';
+import AppAbout from './components/about';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <header id="header">
+        <AppHeader />
+      </header> 
+      <main>
+        <AppHero />
+        <AppAbout />
+        
+      </main>
+      <footer id="footer">
+        <AppFooter />
+      </footer>
+      </div>
   );
 }
 
